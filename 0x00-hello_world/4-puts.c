82a0d681e55970aa91acecb27a813c5d61337922
VictorEkwueme <ekwuemevictor39@gmail.com>
2022-12-08 13:13:32 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,5 @@
 # include <stdio.h>
-
-int main{
-char str1[52]
-strcpy(str1,"Programming is like building a multilingual puzzle'/n);
-puts(str1);
-return 0;
+int main(void){
+	puts ("Programming is like building a multilingual puzzle\n");
+	return 0;
 }
