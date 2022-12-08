68946c89156b79b263a6a80144cb172442a7fb68
VictorEkwueme <ekwuemevictor39@gmail.com>
2022-12-08 12:51:31 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 int main{
 char str1[52]
-strcpy(str1,"Programming is like building a multilingual puzzle');
+strcpy(str1,"Programming is like building a multilingual puzzle'/n);
 puts(str1);
 return 0;
 }
