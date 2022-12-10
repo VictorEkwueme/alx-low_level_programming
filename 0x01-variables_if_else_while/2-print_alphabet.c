a922fab250923a840e53ba94b6f36f1969d141b5
VictorEkwueme <ekwuemevictor39@gmail.com>
2022-12-09 19:04:31 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Entry
+* Return: Always 0
+*/
+int main(void)
+{
+
+	char xy;
+	for (xy ='a', xy <= z, xy++)
+	{
+		putchar(xy);
+	}
+		putchar('\n');
+			return (0);
+}
