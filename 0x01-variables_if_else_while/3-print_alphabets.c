6309ba1441c1e97b458b1553bafba8c2547f5b5d
VictorEkwueme <ekwuemevictor39@gmail.com>
2022-12-09 20:33:28 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - alphabets in lower and upper case
+* Return: Always 0
+*/
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		putchar(lc);
+	}
+	for (lc= 'A'; lc <= 'Z'; lc++)
+	{
+		putchar(lc);
+	}
+	return (0);
