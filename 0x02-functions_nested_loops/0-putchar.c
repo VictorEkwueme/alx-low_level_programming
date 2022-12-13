9982115614c33e0341f56f8c782e40b17d0dc3a1
VictorEkwueme <ekwuemevictor39@gmail.com>
2022-12-13 15:06:40 -0800

    _putchar(_);
    _putchar(p);
    
    _putchar(u);
    
    _putchar(t);
    
    _putchar(c);
    
    _putchar(h);
    
    _putchar(a);
    
    _putchar(r);
    
    _putchar(n);

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <unistd.h>
+#include <stdio.h>
 
 /**
 * main - prints _putchar, followed by a new line
@@ -8,8 +8,15 @@
 int main(void)
 {
 
-	printf("_putchar");
-	printf('\n')
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
