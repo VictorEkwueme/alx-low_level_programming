39dd606ff033b965c01ee660c1103fe5bd4006e2
VictorEkwueme <ekwuemevictor39@gmail.com>
2022-12-14 06:53:10 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,14 +10,15 @@ int print_last_digit(int n)
 
 	int a;
 
-	if (n < 0)
+	if (n < 0 )
 	{
 		n = -n;
-		a = n % 10;
 	}
+
 	if (a < 0)
 	{
-	a = -a;
+		a = -a;
+		a = n % 10;
 	}
 	_putchar(a + '0');
 	return (a);
